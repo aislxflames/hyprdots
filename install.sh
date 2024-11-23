d88bee582691207f62e65e153b9650645ab02e54
Aislx Flames <120901302+aislxflames@users.noreply.github.com>
2024-11-23 20:59:25 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -24,21 +24,32 @@ packages=(
     waypaper
     thunar
     firefox
-    visual-studio-code-bin
-    thunar-devel
+    thunar
     neofetch
     cava
     neo-matrix
     tty-clock
     noto-fonts
     noto-fonts-cjk
-    noto-fonts-full-git
+    noto-fonts-emoji
     rofi-git
     drun
     alacritty
     vim
     nano
-    python-pywal
+    pywal
+    zsh
+    ttf-meslo-nerd-font-powerlevel10k
+    gtk
+    gnome-themes-standard
+    nautilus
+    sassc
+    rsync
+    nwg-look
+    zsh-autosuggestions
+    zsh-syntax-highlighting
+    pokemon-colorscripts-git 
+    sddm
 )
 
 # Function to install yay
@@ -58,7 +69,7 @@ install_yay() {
         echo "Directory /opt/yay already exists. Skipping clone step."
     else
         echo "Cloning the yay repository into /opt..."
-        git clone https://aur.archlinux.org/yay.git /opt/yay
+        sudo git clone https://aur.archlinux.org/yay.git /opt/yay
     fi
 
     # Change ownership of /opt/yay to the current user
@@ -93,33 +104,73 @@ for pkg in "${packages[@]}"; do
     echo "Installing $pkg..."
     yay -S --needed --noconfirm "$pkg"
 done
+    yay -S graphite-gtk-theme-dark-compact
 
 echo "All packages installed successfully!"
 
+echo "Installing gtk themes"
+if [ ! -d /opt/Graphite-gtk-theme ]; then
+  sudo git clone https://github.com/vinceliuice/Graphite-gtk-theme.git /opt/Graphite-gtk-theme
+else
+  echo "/opt/Graphite-gtk-theme already exists, skipping clone."
+fi
+cd /opt/Graphite-gtk-theme 
+sudo ./install.sh  -d ~/.themes/ -t -c dark -s standard -l --tweaks darker rimless
+cd ~
 # Copy .config and Wallpapers directories to the home directory
 USER_HOME="/home/$(logname)"
-SOURCE_CONFIG="./.config"
+SOURCE_CONFIG="./.config/"
 SOURCE_WALLPAPERS="./Wallpapers"
 
 echo "Copying .config and Wallpapers to $USER_HOME..."
+#Installing Oh my zsh
+echo "Installing oh my zsh"
+if [ -d "$HOME/.oh-my-zsh" ]; then
+    echo "~/.oh-my-zsh already exists. Skipping installation."
+else
+    echo "~/.oh-my-zsh does not exist. Installing Oh My Zsh..."
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+echo "Installing plugins"
+sed -i '/^plugins=/,/)/c\plugins=(\n  git\n  zsh-autosuggestions\n  zsh-syntax-highlighting\n)' ~/.zshrc
+echo "Setup pokemon colorscripts"
+if ! grep -q "pokemon-colorscripts -r" ~/.zshrc; then
+    sed -i '1s/^/pokemon-colorscripts -r\n/' ~/.zshrc
+    echo "Added pokemon-colorscripts -r to the beginning of ~/.zshrc"
+else
+    echo "pokemon-colorscripts -r already exists in ~/.zshrc. Skipping."
+fi
+sed -i 's|ZSH_THEME="robbyrussell"|ZSH_THEME="powerlevel10k/powerlevel10k"|g' ~/.zshrc
+
 
 # Ensure the source directories exist
+
+if [ ! -d "$USER_HOME/.config" ]; then
+    mkdir -p "$USER_HOME/.config"
+    echo "Created .config directory at $USER_HOME"
+fi
+
 if [ -d "$SOURCE_CONFIG" ]; then
-    cp -r "$SOURCE_CONFIG" "$USER_HOME"
-    echo "Copied .config to $USER_HOME"
+    rsync -av --ignore-existing "$SOURCE_CONFIG/" "$USER_HOME/.config"
+    echo "Synced .config to $USER_HOME"
 else
     echo "Warning: $SOURCE_CONFIG directory does not exist. Skipping .config copy."
 fi
 
+if [ ! -d "$USER_HOME/Wallpapers" ]; then
+    mkdir -p "$USER_HOME/Wallpapers"
+    echo "Created Wallpapers directory at $USER_HOME"
+fi
+
 if [ -d "$SOURCE_WALLPAPERS" ]; then
-    cp -r "$SOURCE_WALLPAPERS" "$USER_HOME"
-    echo "Copied Wallpapers to $USER_HOME"
+    rsync -av --ignore-existing "$SOURCE_WALLPAPERS/" "$USER_HOME/Wallpapers"
+    echo "Synced Wallpapers to $USER_HOME"
 else
     echo "Warning: $SOURCE_WALLPAPERS directory does not exist. Skipping Wallpapers copy."
 fi
-
 echo "Setup complete! Enjoy your FlamEs Hyprdots!"
-
+#Start sddm
+systemctl enable sddm
 # Ask the user if they want to reboot the system
 read -p "Do you want to reboot your system now? (y/n): " choice
 case "$choice" in
